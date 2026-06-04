d4b06a93efa8d9648309a840b841817f9c887656
Bogusław K. Siemiątkowski <kontakt@spizarniaregio.pl>
2026-06-05 00:30:08 +0200

    aktualizacje toeknów

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,9 +2,9 @@ import { createClient } from "@supabase/supabase-js";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
+const supabasePublishableKey = process.env.EXPO_PUBLIC_SUPABASE_PUBLISHABLE_KEY!;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+export const supabase = createClient(supabaseUrl, supabasePublishableKey, {
   auth: {
     storage: AsyncStorage,
     autoRefreshToken: true,
